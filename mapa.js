fbd15f31c6d1b92315155ec857db596572c216ad
nicolas21746 <nicolas.flores.larama@alumnos.uta.cl>
2025-10-28 19:04:01 -0300

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,5 +1,5 @@
 function iniciarMap(){
-  var coordinates = {lat: ,lgn: };
+  var coordinates = {lat:-20.2327283 ,lgn:-70.1427897 };
   var map = new google.maps.Map(document.getElementById('map'),{
     zoom: 25,
     center : coordinates
