b2b100f091f6271756b54d68aa907f51b2d80ccc
SKILL-XOTHAN <jonathan.choque.mamani@alumnos.uta.cl>
2025-11-10 13:20:42 -0300

    Update poleras.js

--- poleras/poleras.js
+++ poleras/poleras.js
@@ -8,94 +8,89 @@ function agregarCarrito(nombre, precio) {
 
 
 const productos = {
-
+    
     drip: {
         nombre: "Conjunto Drip",
         precio: 35000,
-        img: "../../imga/drip.webp",
+        img: "/othan-store-modificado/imga/drip2.jpg",
         descripcion: `Conjunto drip confeccionado en algodón premium con corte regular fit.
                       Diseño elegante con logo minimalista, ideal para combinar con jeans o pantalones casuales.
                       Perfecta para quienes buscan comodidad y estilo con un toque de lujo.`,
-        tallas: ["S","M","L"],
+        tallas: ["S", "M", "L"],
     },
-    
+
     basicbla: {
-        nombre:"Polera blanca básica",
+        nombre: "Polera blanca básica",
         precio: 15000,
-        img: "../../imga/basicbla.jpg",
+        img: "/othan-store-modificado/imga/basicbla.jpg",
         descripcion: `Polera Tommy blanca confeccionada en algodón premium con corte regular fit.
                       Diseño elegante con logo minimalista, ideal para combinar con jeans o pantalones casuales.
                       Perfecta para quienes buscan comodidad y estilo con un toque de lujo.`,
-        tallas:["S","M","L"]
+        tallas: ["S", "M", "L"]
     },
     mkblanca: {
-        nombre:"Polera Michael Kors blanca",
+        nombre: "Polera Michael Kors blanca",
         precio: 15000,
-        img: "../../imga/mk blanca.jpg",
-        descripcion:`Polera michael kors confeccionada en algodón premium con corte regular fit.
+        img: "/othan-store-modificado/imga/mk blanca.jpg",
+        descripcion: `Polera michael kors confeccionada en algodón premium con corte regular fit.
                      Diseño elegante con logo minimalista, ideal para combinar con jeans o pantalones casuales.
                      Perfecta para quienes buscan comodidad y estilo con un toque de lujo.`,
-        tallas:["S","M","L"]
+        tallas: ["S", "M", "L"]
     },
     bossnegra: {
-        nombre:"Polera Boss negra",
+        nombre: "Polera Boss negra",
         precio: 15000,
-        img: "../../imga/boss.jpg",
-        descripcion:`Polera Boss negra confeccionada en algodón premium con corte regular fit.
+        img: "/othan-store-modificado/imga/boss.jpg",
+        descripcion: `Polera Boss negra confeccionada en algodón premium con corte regular fit.
                      Diseño elegante con logo minimalista, ideal para combinar con jeans o pantalones casuales.
                      Perfecta para quienes buscan comodidad y estilo con un toque de lujo.`,
-        tallas:["S","M","L"]
+        tallas: ["S", "M", "L"]
     },
     mkne: {
-        nombre:"Polera Michael Kors negra",
+        nombre: "Polera Michael Kors negra",
         precio: 15000,
-        img: "../../imga/mk ne.jpg",
-        descripcion:`Polera Michael Kors negra confeccionada en algodón premium con corte regular fit.
+        img: "/othan-store-modificado/imga/mkne",
+        descripcion: `Polera Michael Kors negra confeccionada en algodón premium con corte regular fit.
                      Diseño elegante con logo minimalista, ideal para combinar con jeans o pantalones casuales.
                      Perfecta para quienes buscan comodidad y estilo con un toque de lujo.`,
-        tallas:["S","M","L"]
+        tallas: ["S", "M", "L"]
     },
     mosne: {
-        nombre:"Polera Moschino negra",
+        nombre: "Polera Moschino negra",
         precio: 15000,
-        img: "../../imga/mos ne.jpg",
-        descripcion:`Polera Moschino negra confeccionada en algodón premium con corte regular fit.
+        img: "/othan-store-modificado/imga/mos ne.jpg",
+        descripcion: `Polera Moschino negra confeccionada en algodón premium con corte regular fit.
                      Diseño elegante con logo minimalista, ideal para combinar con jeans o pantalones casuales.
                      Perfecta para quienes buscan comodidad y estilo con un toque de lujo.`,
-        tallas:["S","M","L"]
+        tallas: ["S", "M", "L"]
     },
     tommyne: {
-        nombre:"Polera Tommy negra",
+        nombre: "Polera Tommy negra",
         precio: 15000,
-        img: "../../imga/tommy ne.jpg",
-        descripcion:`Polera Tommy negra confeccionada en algodón premium con corte regular fit.
+        img: "/othan-store-modificado/imga/tommy ne.jpg",
+        descripcion: `Polera Tommy negra confeccionada en algodón premium con corte regular fit.
                      Diseño elegante con logo minimalista, ideal para combinar con jeans o pantalones casuales.
                      Perfecta para quienes buscan comodidad y estilo con un toque de lujo.`,
-        tallas:["S","M","L"]
+        tallas: ["S", "M", "L"]
     },
     tommybl: {
-        nombre:"Polera Tommy blanca",
+        nombre: "Polera Tommy blanca",
         precio: 15000,
-        img: "../../imga/tomy blanca.jpg",
-        descripcion:`Polera Tommy blanca confeccionada en algodón premium con corte regular fit.
+        img: "/othan-store-modificado/imga/tomy blanca.jpg",
+        descripcion: `Polera Tommy blanca confeccionada en algodón premium con corte regular fit.
                      Diseño elegante con logo minimalista, ideal para combinar con jeans o pantalones casuales.
                      Perfecta para quienes buscan comodidad y estilo con un toque de lujo.`,
-        tallas:["S","M","L"]
+        tallas: ["S", "M", "L"]
     },
     aen: {
-        nombre:"Polera Emporio Armani negra",
+        nombre: "Polera Emporio Armani negra",
         precio: 15000,
-        img: "../../imga/aen.jpg",
-        descripcion:`Polera Emporio Armani negra confeccionada en algodón premium con corte regular fit.
+        img: "/othan-store-modificado/imga/aen.jpg",
+        descripcion: `Polera Emporio Armani negra confeccionada en algodón premium con corte regular fit.
                      Diseño elegante con logo minimalista, ideal para combinar con jeans o pantalones casuales.
                      Perfecta para quienes buscan comodidad y estilo con un toque de lujo.`,
-        tallas:["S","M","L"]
+        tallas: ["S", "M", "L"]
     }
-
-
-
-    
-
 };
 
 
@@ -199,5 +194,6 @@ if (producto) {
 } else {
     detalle.innerHTML = "<p>Producto no encontrado.</p>";
 }
+
 
 
